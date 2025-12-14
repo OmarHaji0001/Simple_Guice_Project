dcb9c793d7535b52d082e003d4995d965dd09a4e
OmarHaji0001 <s12029055@stu.najah.edu>
2025-12-14 14:17:17 +0200

    project structure, service interface, service implementation, and pom guice dependency

---
+++ src/main/java/org/example/services/HelloWorldServiceImp.java
@@ -0,0 +1,8 @@
+package org.example.services;
+
+public class HelloWorldServiceImp implements HelloWorldService{
+    @Override
+    public void helloWorld() {
+        System.out.println("Hello World");
+    }
+}
